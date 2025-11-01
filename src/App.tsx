ece2eb646153a2db396526dfa9bda368f94764f5
DanishRafid18 <danishrafid18@gmail.com>
2025-11-01 12:51:44 +1000

    modified so that header is sticky when scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,25 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="py-24 mx-auto max-w-screen-xl ">
-        <div className="flex justify-between gap-4 responsive-wrap">
-          <header className="w-[48%] flex-none justify-between bg-green-500/30 name-div">
+      <div className="mx-auto max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
+        <div className="flex justify-between gap-4 max-w-screen-xl mx-auto responsive-wrap ">
+          <header className="lg:sticky lg:top-0 lg:h-screen lg:flex lg:flex-col lg:justify-between lg:flex-none w-[45%] lg:py-24">
             <h1 className="mb-1 text-2xl font-bold">Danish Rafid Rajendra</h1>
             <h2 className="text-lg font-bold">Software Engineer</h2>
           </header>
 
-          <div className="text-left bg-green-500/3">
+        <div className="text-left lg:py-24">
             <h2 className="text-lg">Software Engineer</h2>
             <p className="">
-              I’m a developer passionate about crafting accessible, pixel-perfect user interfaces that blend thoughtful design with robust engineering. My favorite work lies at the intersection of design and development, creating experiences that not only look great but are meticulously built for performance and usability.
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lobortis eu nibh quis fermentum. Pellentesque venenatis tellus in quam condimentum ultricies. Ut vitae consectetur urna. Nam id ultrices magna, a finibus tortor. Praesent tortor nisl, porta eget est non, ultrices rhoncus enim. Curabitur mattis velit vel nunc cursus hendrerit. Cras at purus vitae nisl auctor viverra eget a sapien. Nulla sit amet augue sollicitudin metus vestibulum auctor. Donec pretium imperdiet lacus nec accumsan. Donec et lorem odio. Pellentesque posuere nisl ac est vulputate semper. Sed dui nulla, rutrum et blandit tincidunt, interdum non est. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+
+Nullam viverra lacus ac lacus blandit, eget consectetur lacus ultrices. Vivamus sed interdum nibh. Etiam sed nunc eu odio fringilla hendrerit sed non velit. Aliquam consectetur, libero sed tempor ornare, mi urna efficitur nisi, porta maximus lacus ex in mauris. Pellentesque ac efficitur odio. Vivamus in facilisis ante. Nullam diam libero, sollicitudin et lobortis quis, aliquet sed enim. In condimentum sit amet mauris vitae ornare. In pulvinar fringilla mollis. Etiam tempor euismod nisi, non tempor dui sagittis ac. Pellentesque ut ante eu tortor tincidunt condimentum et blandit neque.
+
+Nullam ante leo, consequat in diam nec, cursus molestie urna. Vivamus sit amet sapien orci. Proin eget mi eu justo laoreet viverra. Phasellus in laoreet arcu, id molestie odio. Phasellus suscipit dolor et bibendum pellentesque. Pellentesque varius ligula sagittis velit faucibus, sit amet accumsan odio lobortis. Donec tristique maximus accumsan.
+
+Proin semper porta lobortis. Nulla sit amet elit et turpis consectetur posuere ac et velit. Suspendisse potenti. Sed tincidunt ligula at nunc imperdiet gravida. Nam pellentesque ex quis porta ullamcorper. Mauris egestas sagittis ligula eget placerat. In hac habitasse platea dictumst. Morbi ut enim nisl.
+
+Vestibulum viverra magna justo, elementum hendrerit dolor egestas vel. Nullam sit amet ex sapien. Quisque risus arcu, egestas ornare vestibulum eget, euismod in leo. Sed congue venenatis dui, in porttitor est feugiat vitae. Integer erat magna, interdum et tristique eget, fermentum in enim. Ut ac volutpat leo. Donec fermentum luctus ante nec malesuada. Nulla facilisi. Aenean ut tempor tortor, a commodo mauris. Phasellus dui ante, maximus quis vestibulum quis, fringilla id est. Curabitur id posuere dui. Nam eget lobortis lorem. Quisque faucibus tempus velit quis aliquet.
             </p>
           </div>
         </div>
